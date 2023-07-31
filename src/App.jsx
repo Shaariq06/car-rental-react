730006203b4f00495ff0e97170cdc953e63b475c
Shaariq06 <muhammadalishaariq@gmail.com>
2023-07-31 17:42:00 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
 import './App.css';
 import Nav from '/Shaariq/car-rental-react/src/components/nav';
+import Header from './components/header'
+
 
 function App() {
 
   return (
     <section>
       <Nav></Nav>
+      <Header></Header>
     </section>
   )
 }
